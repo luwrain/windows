7664685fa00200044baba7969251191fd275a168
Roman <gr.rPman@gmail.com>
2015-12-07 05:25:13 +0600

    adding support for Desktop default OPEN file action

--- src/main/java/org/luwrain/windows/Windows.java
+++ src/main/java/org/luwrain/windows/Windows.java
@@ -1,7 +1,12 @@
 
 package org.luwrain.windows;
 
+import java.awt.Desktop;
 import java.io.File;
+import java.io.IOException;
+
+import org.luwrain.core.Log;
+import org.luwrain.os.Keyboard;
 
 public class Windows implements org.luwrain.os.OperatingSystem
 {
@@ -16,4 +21,27 @@ public class Windows implements org.luwrain.os.OperatingSystem
     {
 	return hardware;
     }
+
+	@Override public void fileOpendDesktopDefault(File file)
+	{
+		if(!Desktop.isDesktopSupported())
+		{
+			throw new UnsupportedOperationException("This OS does not support for Desktop");
+		}
+		Desktop desktop = Desktop.getDesktop();
+		if(!desktop.isSupported(Desktop.Action.OPEN))
+		{
+			throw new UnsupportedOperationException("This OS does not support for Desktop action OPEN");
+		}
+		try
+		{
+			desktop.open(file);
+		}
+		catch(Exception e)
+		{
+			// FEXME: make better error handling
+			Log.debug("windows",e.getMessage());
+		}
+	}
+
 }
