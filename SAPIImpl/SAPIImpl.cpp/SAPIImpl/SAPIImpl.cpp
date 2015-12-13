0dd8c24c014f6a2f7edeeba323f67ec50b871bd2
Roman <gr.rPman@gmail.com>
2015-12-13 21:17:50 +0600

    SAPI jni, added support for speak to wav file and make it possible to do it async, each speak in the same java object

--- SAPIImpl/SAPIImpl.cpp/SAPIImpl/SAPIImpl.cpp
+++ SAPIImpl/SAPIImpl.cpp/SAPIImpl/SAPIImpl.cpp
@@ -6,6 +6,8 @@
 #pragma warning (disable:4996) 
 #include <sphelper.h>
 
+#include <map>
+
 #include "org_luwrain_windows_speech_SAPIImpl.h"
 
 // классы помошники
@@ -22,6 +24,9 @@ public:
 	CComPtr<ISpObjectToken> cpToken = NULL;
 	CComPtr<ISpVoice> cpVoice;
 	HRESULT hr;
+	
+	CSpStreamFormat cAudioFmt;
+	CComPtr<ISpStream> cpStream = NULL;
 
 	void init()
 	{
@@ -32,6 +37,21 @@ public:
 		//
 		hr = CoCreateInstance(CLSID_SpVoice, NULL, CLSCTX_ALL, IID_ISpVoice, (void **)&cpVoice);
 		//hr = SpCreateBestObject(SPCAT_VOICES, NULL, NULL, &cpVoice);
+	}
+	~SAPIImpl()
+	{
+		if (cpStream != NULL)
+		{
+			hr = cpStream->Close();
+			cpStream.Release();
+			cpStream = NULL;
+		}
+		if (cpToken != NULL)
+		{
+			cpToken.Release(); cpToken = NULL;
+			cpIEnum.Release();
+		}
+		cpVoice.Release();
 	}
 	jint searchVoiceToken(jchar* cond)
 	{
@@ -82,13 +102,79 @@ public:
 		return 0;
 	}
 
+	jint stream(jchar* stream, jint flags)
+	{
+		if (stream == NULL)
+		{
+			if (cpStream != NULL) { hr = cpStream->Close(); cpStream.Release(); cpStream = NULL; }
+			hr = cpVoice->SetOutput(NULL, TRUE);
+			if (hr != S_OK) return 3;
+		}
+		else
+		{
+			hr = cAudioFmt.AssignFormat((SPSTREAMFORMAT)flags);
+			if (hr != S_OK) return 1;
+			if (cpStream != NULL) { hr = cpStream->Close(); cpStream.Release(); cpStream = NULL; }
+			hr = SPBindToFile((LPWSTR)stream, SPFM_CREATE_ALWAYS, &cpStream, &cAudioFmt.FormatId(), cAudioFmt.WaveFormatExPtr());
+			if (hr != S_OK) return 2;
+			hr = cpVoice->SetOutput(cpStream, TRUE);
+			if (hr != S_OK) return 3;
+		}
+		return 0;
+	}
+
+	jint set_rate(jint rate)
+	{ // -10 .. 10
+		hr = cpVoice->SetRate(rate);
+		if (hr != S_OK) return 1;
+		return 0;
+	}
+	jint set_pitch(jint pitch)
+	{ // 0..100
+		hr = cpVoice->SetVolume((USHORT)pitch);
+		if (hr != S_OK) return 1;
+		return 0;
+	}
+	jint wait_done(jint timeout)
+	{
+		hr = cpVoice->WaitUntilDone(timeout);
+		if (hr != S_OK) return 1;
+		return 0;
+	}
 };
 
-SAPIImpl impl;
+bool doStaticInit = true;
+jfieldID idField;
+std::map<int, SAPIImpl*> sList;
 
-JNIEXPORT jstring JNICALL Java_org_luwrain_windows_speech_SAPIImpl_getNextVoiceIdFromList(JNIEnv * jni, jclass jthis)
+SAPIImpl& make_impl(JNIEnv * jni,jobject &jthis)
 {
+	if (doStaticInit)
+	{
+		jclass c = jni->GetObjectClass(jthis);
+		idField = jni->GetFieldID(c, "id", "I");
+	}
+	int id = jni->GetIntField(jthis, idField);
+
+//	printf("make_impl(%d)\n", id);
+	if (sList.find(id) == sList.end()) sList[id] = new SAPIImpl();
+	SAPIImpl& impl = *sList[id];
 	impl.init();
+	return impl;
+}
+void release_impl(JNIEnv * jni,jobject &jthis)
+{
+	int id = jni->GetIntField(jthis, idField);
+	std::map<int, SAPIImpl*>::iterator it = sList.find(id);
+
+	if (it == sList.end()) return;
+	delete it->second;
+	sList.erase(it);
+}
+
+JNIEXPORT jstring JNICALL Java_org_luwrain_windows_speech_SAPIImpl_getNextVoiceIdFromList(JNIEnv * jni, jobject jthis)
+{
+	SAPIImpl& impl = make_impl(jni,jthis);
 	//
 	LPWSTR id = NULL;
 	new CoAutocleaner(id); // чуть раньше времени задаем метод автоочистки памяти
@@ -97,16 +183,16 @@ JNIEXPORT jstring JNICALL Java_org_luwrain_windows_speech_SAPIImpl_getNextVoiceI
 	// формируем строку для возврата в java
 	return jni->NewString((jchar*)id, (jsize)wcslen(id));
 }
-JNIEXPORT jint JNICALL Java_org_luwrain_windows_speech_SAPIImpl_selectCurrentVoice(JNIEnv * jni, jclass jthis)
+JNIEXPORT jint JNICALL Java_org_luwrain_windows_speech_SAPIImpl_selectCurrentVoice(JNIEnv * jni, jobject jthis)
 {
-	impl.init();
+	SAPIImpl& impl = make_impl(jni, jthis);
 	//
 	// выбираем текущий токен
 	return impl.selectCurrentVoice();
 }
-JNIEXPORT jint JNICALL Java_org_luwrain_windows_speech_SAPIImpl_selectVoiceById(JNIEnv * jni, jclass jthis, jstring jtext)
+JNIEXPORT jint JNICALL Java_org_luwrain_windows_speech_SAPIImpl_selectVoiceById(JNIEnv * jni, jobject jthis, jstring jtext)
 {
-	impl.init();
+	SAPIImpl& impl = make_impl(jni, jthis);
 	//
 	// выгружаем строку с условием
 	jsize length = jni->GetStringLength(jtext);
@@ -117,9 +203,9 @@ JNIEXPORT jint JNICALL Java_org_luwrain_windows_speech_SAPIImpl_selectVoiceById(
 	// выбираем токен по идентификатору
 	return impl.selectVoiceById(text);
 }
-JNIEXPORT jint JNICALL Java_org_luwrain_windows_speech_SAPIImpl_searchVoiceByAttributes(JNIEnv * jni, jclass jthis, jstring jtext)
+JNIEXPORT jint JNICALL Java_org_luwrain_windows_speech_SAPIImpl_searchVoiceByAttributes(JNIEnv * jni, jobject jthis, jstring jtext)
 {
-	impl.init();
+	SAPIImpl& impl = make_impl(jni, jthis);
 	//
 	// если критерии не указаны (выдан null) то делаем запрос на все токены
 	if (jtext == NULL) return impl.searchVoiceToken(NULL);
@@ -132,9 +218,9 @@ JNIEXPORT jint JNICALL Java_org_luwrain_windows_speech_SAPIImpl_searchVoiceByAtt
 	// запрос на токены по критериям
 	return impl.searchVoiceToken(text);
 }
-JNIEXPORT jint JNICALL Java_org_luwrain_windows_speech_SAPIImpl_speak(JNIEnv * jni, jclass jthis, jstring jtext, jint flags)
+JNIEXPORT jint JNICALL Java_org_luwrain_windows_speech_SAPIImpl_speak(JNIEnv * jni, jobject jthis, jstring jtext, jint flags)
 {
-	impl.init();
+	SAPIImpl& impl = make_impl(jni, jthis);
 	// выгружаем строку с условием
 	jsize length = jni->GetStringLength(jtext);
 	jchar* text = new jchar[length + 1];
@@ -143,4 +229,39 @@ JNIEXPORT jint JNICALL Java_org_luwrain_windows_speech_SAPIImpl_speak(JNIEnv * j
 	text[length] = L'\x0';
 	// говорим
 	return impl.speak(text, flags);
+}
+
+JNIEXPORT jint JNICALL Java_org_luwrain_windows_speech_SAPIImpl_stream(JNIEnv * jni, jobject jthis, jstring jstream, jint flags)
+{
+	SAPIImpl& impl = make_impl(jni, jthis);
+	// выгружаем строку с условием
+	jchar* stream = NULL;
+	if (jstream != NULL)
+	{
+		jsize length = jni->GetStringLength(jstream);
+		stream = new jchar[length + 1];
+		new Autocleaner(stream);
+		jni->GetStringRegion(jstream, 0, length, stream);
+		stream[length] = L'\x0';
+	}
+	// устанавливаем, куда выводим звук (null - звуковая карта или путь к wav файлу)
+	return impl.stream(stream, flags);
+}
+
+JNIEXPORT jint JNICALL Java_org_luwrain_windows_speech_SAPIImpl_rate(JNIEnv * jni, jobject jthis, jint rate)
+{
+	SAPIImpl& impl = make_impl(jni, jthis);
+	return impl.set_rate(rate);
+}
+
+JNIEXPORT jint JNICALL Java_org_luwrain_windows_speech_SAPIImpl_pitch(JNIEnv * jni, jobject jthis, jint pitch)
+{
+	SAPIImpl& impl = make_impl(jni, jthis);
+	return impl.set_pitch(pitch);
+}
+
+JNIEXPORT jint JNICALL Java_org_luwrain_windows_speech_SAPIImpl_wait(JNIEnv * jni, jobject jthis, jint timeout)
+{
+	SAPIImpl& impl = make_impl(jni, jthis);
+	return impl.wait_done(timeout);
 }
